44836e6c163f16b38cfd071f9711d80febb5fd91
feizhaicode <70507003+feizhaicode@users.noreply.github.com>
2020-08-31 19:20:23 +0000

    Update v2ray-manage.sh

--- v2ray/v2ray-manage.sh
+++ v2ray/v2ray-manage.sh
@@ -5,12 +5,9 @@ echo "Select:"
 echo "1.create new user"
 read select
 if [ $select == 1 ];then
-	echo "select a port:"
-#grep 找到"port":那一行 ,awk 使用 :和, 分割打印第二个
-	cat $file|grep "\"port\":" |awk -F '[:,]' '{print $2}'
-	read num
 	uuid=`/usr/local/bin/v2ctl uuid`
-	sed -i '/"port": '$num'/a\{"id": "'$uuid'","alterId": 0}' $file	
+	sed -i '/"clients"/a\{"id": "'$uuid'","alterId": 0}' $file	
+	echo "done"
 else
 	echo "Not found:$select"
 	
