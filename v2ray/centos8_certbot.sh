d95012da21bc9a742548adb683097dd88b17c2c7
feizhaicode <70507003+feizhaicode@users.noreply.github.com>
2020-08-31 21:23:28 +0000

    Update centos8_certbot.sh

--- v2ray/centos8_certbot.sh
+++ v2ray/centos8_certbot.sh
@@ -1,2 +1,4 @@
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install certbot python3-certbot-nginx
+#certbot --nginx
+#echo "0 0,12 * * * root python3 -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
