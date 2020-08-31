b354528b3c6d50c186a4efb36746f7a272835c3e
feizhaicode <70507003+feizhaicode@users.noreply.github.com>
2020-08-31 18:29:22 +0000

    Create download.sh

---
+++ v2ray/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -O https://raw.githubusercontent.com/feizhaicode/myconfigure/master/v2ray/install.sh
+curl -O https://raw.githubusercontent.com/feizhaicode/myconfigure/master/v2ray/config.sh
+curl -O https://raw.githubusercontent.com/feizhaicode/myconfigure/master/v2ray/v2ray-manage.sh
