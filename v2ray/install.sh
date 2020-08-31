154b42530a8df0ad715d30170b0a275093b907fe
feizhaicode <70507003+feizhaicode@users.noreply.github.com>
2020-08-31 19:36:18 +0000

    Update install.sh

--- v2ray/install.sh
+++ v2ray/install.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "开始安装"
 yum install -y nginx
-systemctl start nginx.service
 systemctl enable nginx.service
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --zone=public --add-port=443/tcp --permanent
@@ -13,5 +12,4 @@ curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install
 curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
 bash install-release.sh
 bash install-dat-release.sh
-systemctl start v2ray.service
 systemctl enable v2ray.service
