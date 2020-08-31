cf1fd834567e29ce7261994fe9b95584566143ba
feizhaicode <70507003+feizhaicode@users.noreply.github.com>
2020-08-31 17:36:15 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "开始安装"
+yum install -y nginx
+systemctl start nginx.service
+systemctl enable nginx.service
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=443/tcp --permanent
+firewall-cmd --reload
+
+curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+bash install-release.sh
+bash install-dat-release.sh
