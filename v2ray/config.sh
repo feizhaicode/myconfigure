835294d98a786016209a13e3696a4542125afb66
feizhaicode <70507003+feizhaicode@users.noreply.github.com>
2020-08-31 19:50:05 +0000

    Update config.sh

--- v2ray/config.sh
+++ v2ray/config.sh
@@ -26,11 +26,11 @@ cat <<done>>$NginxFilePath
 		proxy_redirect off;
 		proxy_pass http://127.0.0.1:35861;
 		proxy_http_version 1.1;
-		proxy_set_header Upgrade $http_upgrade;
+		proxy_set_header Upgrade \$http_upgrade;
 	       	proxy_set_header Connection "upgrade";
-		proxy_set_header Host $http_host;
-		proxy_set_header X-Real-IP $remote_addr;
-		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+		proxy_set_header Host \$http_host;
+		proxy_set_header X-Real-IP \$remote_addr;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 	}
     access_log  /var/log/nginx/feizhai.access.log;
     error_log /var/log/nginx/feizhai.error.log;
